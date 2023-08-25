f5b4fd2a1056eb37a25a913d90fe917b433a6fd4
Alex Schlögl <a.schloegl@pm.me>
2023-08-25 21:10:21 +0200

    continue debugging a bit

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # ML introspection
 
-- [ ] load pretrained model (cifar10 or smth)
-- [ ] generate seeded random input
-- [ ] run model on input
-- [ ] check that results are deterministic
+- [x] load pretrained model (cifar10 or smth)
+- [x] generate seeded random input
+- [x] run model on input
+- [x] check that results are deterministic
 - [ ] examine generated C code
 - [ ] examine generated assembly
 - [ ] automate the introspection (parse AST?)
--- test.py
+++ test.py
@@ -1,3 +1,36 @@
-#%%
-import tinygrad
+# %%
+from tinygrad.nn import Conv2d, Linear
+
+
+class TestModel:
+    def __init__(self):
+        self.l1 = Conv2d(3, 3, 3)
+        self.l2 = Linear(2700, 10)
+
+    def __call__(self, x):
+        x = self.l1(x)
+        x = x.relu()
+        x = x.flatten()
+        x = self.l2(x)
+
+        return x
+
+
+# %%
 import numpy as np
+
+
+from tinygrad.tensor import Tensor
+from tinygrad.jit import TinyJit
+
+@TinyJit
+def jit(x):
+    m = TestModel()
+    outputs = m(x)
+    print(outputs.realize().numpy())
+
+np.random.seed(1337)
+inputs = np.random.randn(1, 3, 32, 32)
+input_tensor = Tensor(inputs)
+    
+jit(input_tensor)   
